4dcdf89fedafb379dea058c9d09d550b3bbded8e
bumi95 <39798011+bumi95@users.noreply.github.com>
2021-06-27 20:07:18 +0900

    Update TreeNode.java

--- Huffman_Tree/TreeNode.java
+++ Huffman_Tree/TreeNode.java
@@ -1,12 +1,14 @@
-public class TreeNode { // 히프와 huffman tree에 사용할 트리노드 객체
-  public int num; // 빈도 수
-  public char word; // 문자
-  public TreeNode leftNode;
-  public TreeNode rightNode;
-  
-  public TreeNode(int num, char word) {
-    this.num = num;
-    this.word = word;
-    leftNode = rightNode = null;
-  }
+public class TreeNode { // 히프와 huffman트리에 사용할 트리노드 객체
+	
+	public int num; // 빈도 수 
+	public char word; // 문자
+	public TreeNode leftNode;
+	public TreeNode rightNode;
+	
+	public TreeNode(int num, char word) {
+		
+		this.num = num;
+		this.word = word;
+		leftNode = rightNode = null;
+	}
 }
